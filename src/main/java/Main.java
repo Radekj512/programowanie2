df6d4411df5004384d3c1d60dc55d11932358963
Radek <roszko.radek@gmail.com>
2019-05-11 12:11:18 +0200

    Added menu in main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder menu = new StringBuilder();
+        menu.append("//--------------------------------------//\n");
+        menu.append("//1. Wyświetl listę książek             //\n");
+        menu.append("//2. Wyjście                            //\n");
+        menu.append("//--------------------------------------//\n");
 
+        System.out.println(menu);
+    }
+}
